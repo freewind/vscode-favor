60decedb04b000336fab81051431ea2b6429b8ab
Freewind <nowindlee@gmail.com>
2024-11-03 14:59:19 +0800

    希望增加分组功能，在右键菜单中加入 add to favorite group，并且支持对favor分组

--- extension.js
+++ extension.js
@@ -7,6 +7,7 @@ class FavoritesProvider {
         this._onDidChangeTreeData = new vscode.EventEmitter();
         this.onDidChangeTreeData = this._onDidChangeTreeData.event;
         this.favorites = new Map();
+        this.groups = new Map();
         this.context = context;
         this.view = null;
         this.loadFavorites();
@@ -17,14 +18,35 @@ class FavoritesProvider {
         favorites.forEach(item => {
             this.favorites.set(item.path, item);
         });
+        
+        const groups = this.context.globalState.get('favoriteGroups', {});
+        Object.entries(groups).forEach(([groupName, items]) => {
+            this.groups.set(groupName, new Map(items.map(item => [item.path, item])));
+        });
     }
 
     saveFavorites() {
         const favoritesArray = Array.from(this.favorites.values());
+        const groupsObject = {};
+        this.groups.forEach((items, groupName) => {
+            groupsObject[groupName] = Array.from(items.values());
+        });
+        
         this.context.globalState.update('favorites', favoritesArray);
+        this.context.globalState.update('favoriteGroups', groupsObject);
     }
 
     getTreeItem(element) {
+        if (element.isGroup) {
+            const treeItem = new vscode.TreeItem(
+                element.name,
+                vscode.TreeItemCollapsibleState.Expanded
+            );
+            treeItem.contextValue = 'group';
+            treeItem.iconPath = new vscode.ThemeIcon('folder');
+            return treeItem;
+        }
+
         const treeItem = new vscode.TreeItem(
             element.name,
             element.type === 'folder' ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None
@@ -45,8 +67,19 @@ class FavoritesProvider {
 
     getChildren(element) {
         if (!element) {
-            return Array.from(this.favorites.values());
+            const defaultItems = Array.from(this.favorites.values());
+            const groups = Array.from(this.groups.keys()).map(name => ({
+                name,
+                isGroup: true
+            }));
+            return [...defaultItems, ...groups];
         }
+        
+        if (element.isGroup) {
+            const groupItems = this.groups.get(element.name);
+            return groupItems ? Array.from(groupItems.values()) : [];
+        }
+        
         return [];
     }
 
@@ -66,6 +99,43 @@ class FavoritesProvider {
         this.favorites.delete(item.path);
         this.saveFavorites();
         this._onDidChangeTreeData.fire();
+    }
+
+    async addToGroup(uri, groupName) {
+        if (!groupName) {
+            const result = await vscode.window.showInputBox({
+                placeHolder: "Enter group name",
+                prompt: "Enter a name for the favorite group"
+            });
+            if (!result) return;
+            groupName = result;
+        }
+
+        if (!this.groups.has(groupName)) {
+            this.groups.set(groupName, new Map());
+        }
+
+        const stat = fs.statSync(uri.fsPath);
+        const favorite = {
+            path: uri.fsPath,
+            name: path.basename(uri.fsPath),
+            type: stat.isDirectory() ? 'folder' : 'file'
+        };
+
+        this.groups.get(groupName).set(uri.fsPath, favorite);
+        this.saveFavorites();
+        this._onDidChangeTreeData.fire();
+    }
+
+    removeFromGroup(item, groupName) {
+        if (this.groups.has(groupName)) {
+            this.groups.get(groupName).delete(item.path);
+            if (this.groups.get(groupName).size === 0) {
+                this.groups.delete(groupName);
+            }
+            this.saveFavorites();
+            this._onDidChangeTreeData.fire();
+        }
     }
 
     setTreeView(view) {
@@ -128,13 +198,30 @@ function activate(context) {
         }
     });
 
+    let addToGroup = vscode.commands.registerCommand('vscode-favorites.addToGroup', async (uri) => {
+        if (!uri) {
+            uri = vscode.window.activeTextEditor?.document.uri;
+        }
+        if (uri) {
+            await favoritesProvider.addToGroup(uri);
+        }
+    });
+
+    let removeFromGroup = vscode.commands.registerCommand('vscode-favorites.removeFromGroup', (item, groupName) => {
+        if (item && groupName) {
+            favoritesProvider.removeFromGroup(item, groupName);
+        }
+    });
+
     context.subscriptions.push(
         treeView,
         addToFavorites,
         removeFromFavorites,
         openFavoriteFiles,
         openSelectedFiles,
-        openFavoriteFile
+        openFavoriteFile,
+        addToGroup,
+        removeFromGroup
     );
 }
 
